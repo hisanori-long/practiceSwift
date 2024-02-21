8cf555718f0f7491e1969ad165309f19268b146a
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-02-21 22:06:02 +0900

    Bool型

--- Lesson3.playground/Contents.swift
+++ Lesson3.playground/Contents.swift
@@ -1,1 +1,24 @@
+import UIKit
 
+"""
+Bool型
+"""
+// 変数の定義
+let a = true
+let b = false
+
+// 否定
+print(!a)
+print(!b)
+
+// 論理積（両方ともtrueならtrue）
+print(false && false)
+print(false && true)
+print(true && false)
+print(true && true)
+
+// 論理和（どちらか一つがtrueならtrue）
+print(false || false)
+print(false || true)
+print(true || false)
+print(true || true)
