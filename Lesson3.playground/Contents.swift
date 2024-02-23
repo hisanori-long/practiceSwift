3cc3bee00aa89c25aef5271db5b3108e0e689a69
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-02-23 13:50:02 +0900

    Optional<Wrapped>型

--- Lesson3.playground/Contents.swift
+++ Lesson3.playground/Contents.swift
@@ -144,14 +144,98 @@ order == ComparisonResult.orderedSame // orderedSame -> 順序が同じならtru
 // 文字列の探索
 "abc".range(of: "bc") // 0から数え始めて1番目から2番目に位置する
 
+"""
+Optional<Wrapped>型
+"""
+//enum Optional<Wrapped> { // 列挙型
+//    case none // 値が不在の場合
+//    case some(Wrapped) // 値が存在する場合
+//}
 
+let none = Optional<Int>.none
+print(".none: \(String(describing: none))")
+let some = Optional<Int>.some(1)
+print(".some: \(String(describing: some))")
 
+// 型推論
+print(Optional.some(1)) // Optional<Int>型だと推論
+//print(Optional.none) // .noneは型の推論ができないので、明示的に示す必要がある
 
+// シンプルな型の値の生成
+var t: Int?
+t = nil // nilリテラルを代入し、.noneの生成
+t = Optional(1) // イニシャライザによる、.someの生成
+t = 1 // 値を代入し、.someの生成
 
+// nilリテラルの代入による.noneの生成
+let optionalIntNil: Int? = nil
+let optionalStringNil: String? = nil
+print(type(of: optionalIntNil), String(describing: optionalIntNil))
+print(type(of: optionalStringNil), String(describing: optionalStringNil))
 
+// イニシャライザいよる.someの生成
+let optionalIntSome = Optional(1)
+let optionalStringSome = Optional("a")
+print(type(of: optionalIntSome), String(describing: optionalIntSome))
+print(type(of: optionalStringSome), String(describing: optionalStringSome))
 
+// 値の代入による.someの生成（Int?型を使用）
+let optionalInt: Int? = 1
+print(type(of: optionalInt), String(describing: optionalInt))
 
+//型のアンラップ（Optional<Wrapped>型の値からWrapped型の値を取り出すこと）
+// Int?同士の四則演算は、値を持っていない可能性があるため、コンパイルエラーになる
+let a_Optional: Int? = 1
+let b_Optional: Int? = 1
+//a + b // コンパイルエラー
 
+// オプショナルバインディング（if文による値の取り出し）
+let OptionalA = Optional("a")
+if let a = OptionalA {
+    print(type(of: a))
+}
+
+//??演算子（値が存在しない場合のデフォルトの値を指定する）
+let optionalInt2: Int? = 1
+let int2 = optionalInt2 ?? 3 // 値が存在したので、そのまま返す
+let optionalInt3: Int? = nil
+let int3 = optionalInt3 ?? 3 // 値が存在しないので、デフォルトの値を返す
+
+// 強制アンラップ（あたいが存在しなければ、実行時エラーになる）
+let u: Int? = 1
+let v: Int? = 1
+u! + v!
+
+//オプショナルチェイン（アンラップを使用せずに値のプロパティやメソッドにアクセス）
+let optionalDouble = Optional(1.0)
+let optionalIsInfinite = optionalDouble?.isInfinite //あたいが存在しなければnilを返す
+print(String(describing: optionalIsInfinite))
+
+let optionalRange = Optional(0..<10)
+let containsSeven = optionalRange?.contains(7)
+print(String(describing: containsSeven))
+
+// map(_:)メソッドとflatMap(_:)メソッド（アンラップを伴わずに値の変更を行う）
+let w = Optional(17)
+let x = w.map({ value in value * 2})
+print(type(of: x))
+
+let aa = Optional("17")
+let ab = aa.map({ value in Int(value)}) // mapでは、Optional<Wrapped>が２重になってしまう
+print(type(of: ab)) // Optional<Optional<Int>>.Ty>>
+let ac = aa.flatMap({ value in Int(value)}) // flatMapによりOptional<Wrapped>が２重になることを防ぐ
+print(type(of: ac)) // Optional<Int>.Typ>
+
+// 暗黙的にアンラップされたOptional<Wrapped>型（Wrapped!型）
+var ad: String? = "a"
+var ae: String! = "b" // 暗黙的にアンラップする
+print(type(of: ad))
+print(type(of: ae))
+var af: String! = ad // アンラップしていないので、アンラップが必要
+var ag: String? = ae // 暗黙的にアンラップしているので、アンラップは不要
+
+var ah: Int! = nil
+//ah + 1 // Intに値がないため、実行時にエラーが起きる
 
 
 
